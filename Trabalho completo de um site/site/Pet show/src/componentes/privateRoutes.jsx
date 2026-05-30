38977652c11da5a52a4527605a0f10a43a9a2d13
Alice Lacerda <alice1.137.3.14@gmail.com>
2026-05-30 06:33:29 -0300

    docs: Mudando pasta de arquivo para outro repositorio proprio.

--- Trabalho completo de um site/site/Pet show/src/componentes/privateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { Navigate } from "react-router-dom";
-import { UserContext } from "../componentes/userContext";
-
-export default function PrivateRoute({ children }) {
-  const { usuario } = useContext(UserContext);
-
-  return usuario ? children : <Navigate to="/login" replace />;
-}
